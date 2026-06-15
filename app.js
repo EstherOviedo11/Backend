cd7ffe326d76eb9acfc218f9371c8f9916c130ab
EstherOviedo11 <esther2111121@gmail.com>
2026-06-15 00:42:36 -0600

    Actualizacion en app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express= require('express');
 const morgan = require('morgan');
 const path= require('path');
-const mysql= require('mysql');
+const mysql= require('mysql2');
 const cors = require('cors');
 const myConnection= require('express-myconnection');
 const app= express();
@@ -26,7 +26,7 @@ app.use(cors(
 
 
 // rutas backend
-const bancosRoutes = require('./rutas/bancos');
+//const bancosRoutes = require('./rutas/bancos');
 
 app.set('port', process.env.PORT || 3000);
 
@@ -34,9 +34,9 @@ app.use(morgan('dev'));
 app.use(myConnection(mysql,{
     host:'localhost',
     user:'root',
-    password:'Sierra84$',
+    password:'Kogami401509',
     port:3306,
-    database:'academics'
+    database:'ventas'
 }, 'single'));
 app.use(express.urlencoded({extended: false}));
 
